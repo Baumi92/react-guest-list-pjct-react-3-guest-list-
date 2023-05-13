8ccdee6ff4e62eebae85ddfcf5c9721e618d7f0a
Manuel Baumgartner <manu.baumi@outlook.com>
2023-05-13 13:07:50 +0200

    Add framework

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import './App.css';
+import { useState } from 'react';
 
-export default function App() {
+export default function ExampleFormsWithControlledComponents() {
+  const [label, setLabel] = useState('');
+  // 1. Create the state variable
+  const [usernameInput, setUsernameInput] = useState('');
+  const [isCookiePolicyAccepted, setIsCookiePolicyAccepted] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <label>
+        Cooke policy accepted?
+        {/* // 2.  use the current value of the state as the checked property of the input*/}
+        <input
+          checked={isCookiePolicyAccepted}
+          type="checkbox"
+          // 3. update the state value with the event.currentTarget.checked
+          onChange={(event) => {
+            setIsCookiePolicyAccepted(event.currentTarget.checked);
+          }}
+        />
+        <button>Accept</button>
+      </label>
+      onSubmit=
+      {(event) => {
+        event.preventDefault();
+      }}
+      {/* Boolean operator */}
+      {label !== '' && <p>Username: {label}</p>}
+      {/* 2. Connect the state with the input value */}
+      <input
+        value={usernameInput}
+        onChange={(event) => {
+          // 3. update state on change event
+          setUsernameInput(event.currentTarget.value);
+        }}
+      />
+      {/* Ternary operator */}
+      {usernameInput === '' ? (
+        ' please type ⌨️'
+      ) : (
+        <button
+          onClick={() => {
+            setLabel(usernameInput);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          update label
+        </button>
+      )}
+    </form>
   );
 }
