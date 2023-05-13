b5c1e4f681decc0ecbef5b628468a0d2ec2d512e
Manuel Baumgartner <manu.baumi@outlook.com>
2023-05-13 13:45:44 +0200

    Add array of objects

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import './App.css';
-import { useState } from 'react';
+import ExampleArrayOfObjectsInReact from './Arrayofobjectsinreact';
+import ExampleControlledComponentsCheckboxes from './Checkbox';
+import ExampleFetchingInitialState from './Fetchinitialstate';
 
-export default function ExampleFormsWithControlledComponents() {
-  const [label, setLabel] = useState('');
-  // 1. Create the state variable
-  const [usernameInput, setUsernameInput] = useState('');
-  const [isCookiePolicyAccepted, setIsCookiePolicyAccepted] = useState(false);
+function App() {
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
-      <label>
-        Cooke policy accepted?
-        {/* // 2.  use the current value of the state as the checked property of the input*/}
-        <input
-          checked={isCookiePolicyAccepted}
-          type="checkbox"
-          // 3. update the state value with the event.currentTarget.checked
-          onChange={(event) => {
-            setIsCookiePolicyAccepted(event.currentTarget.checked);
-          }}
-        />
-        <button>Accept</button>
-      </label>
-      onSubmit=
-      {(event) => {
-        event.preventDefault();
-      }}
-      {/* Boolean operator */}
-      {label !== '' && <p>Username: {label}</p>}
-      {/* 2. Connect the state with the input value */}
-      <input
-        value={usernameInput}
-        onChange={(event) => {
-          // 3. update state on change event
-          setUsernameInput(event.currentTarget.value);
-        }}
-      />
-      {/* Ternary operator */}
-      {usernameInput === '' ? (
-        ' please type ⌨️'
-      ) : (
-        <button
-          onClick={() => {
-            setLabel(usernameInput);
-          }}
-        >
-          update label
-        </button>
-      )}
-    </form>
+    <div>
+      <h1> Guest List</h1>
+      <ExampleControlledComponentsCheckboxes />
+      <ExampleFetchingInitialState />
+      <ExampleArrayOfObjectsInReact />
+    </div>
   );
 }
+
+export default App;
