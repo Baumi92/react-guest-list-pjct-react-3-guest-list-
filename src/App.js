1e30bf53acf3898fa4abd8f0cec9e1a6676826b3
Manuel Baumgartner <manu.baumi@outlook.com>
2023-05-15 23:37:59 +0200

    Add api host

--- src/App.js
+++ src/App.js
@@ -1,105 +1,147 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
   const [guests, setGuests] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [loading, setLoading] = useState(true);
+  const baseUrl = 'https://localhost:4000';
+
+  const loadGuests = async () => {
+    try {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      setGuests(allGuests);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get('https://my-api.com/guests')
-      .then((response) => {
-        setGuests(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
+    loadGuests().catch((error) => {
+      console.log(error);
+    });
   }, []);
 
-  const handleAddGuest = (event) => {
-    event.preventDefault();
-    const newGuest = {
-      firstName: firstName,
-      lastName: lastName,
-      attending: false,
-    };
-    axios
-      .post('https://my-api.com/guests', newGuest)
-      .then((response) => {
-        setGuests([...guests, response.data]);
+  const addGuest = async () => {
+    if (firstName && lastName) {
+      try {
+        const response = await fetch(`${baseUrl}/guests`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ firstName, lastName }),
+        });
+        const createdGuest = await response.json();
+        setGuests([...guests, createdGuest]);
         setFirstName('');
         setLastName('');
-      })
-      .catch((error) => {
+      } catch (error) {
         console.log(error);
-      });
-  };
-
-  const handleDeleteGuest = (id) => {
-    axios
-      .delete(`https://my-api.com/guests/${id}`)
-      .then(() => {
-        setGuests(guests.filter((guest) => guest.id !== id));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const handleToggleAttending = (id) => {
-    const updatedGuests = guests.map((guest) => {
-      if (guest.id === id) {
-        return {
-          ...guest,
-          attending: !guest.attending,
-        };
-      } else {
-        return guest;
       }
-    });
-    axios
-      .put(
-        `https://my-api.com/guests/${id}`,
-        updatedGuests.find((guest) => guest.id === id),
-      )
-      .then(() => {
-        setGuests(updatedGuests);
-      })
-      .catch((error) => {
-        console.log(error);
+    }
+  };
+
+  const deleteGuest = async (guestId) => {
+    try {
+      await fetch(`${baseUrl}/guests/${guestId}`, { method: 'DELETE' });
+      setGuests(guests.filter((guest) => guest.id !== guestId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const toggleAttending = async (guestId) => {
+    try {
+      const updatedGuests = guests.map((guest) => {
+        if (guest.id === guestId) {
+          return {
+            ...guest,
+            attending: !guest.attending,
+          };
+        }
+        return guest;
       });
+
+      await fetch(`${baseUrl}/guests/${guestId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ attending: !guestId.attending }),
+      });
+      setGuests(updatedGuests);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
   };
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
   };
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleKeyPress = async (e) => {
+    if (e.key === 'Enter') {
+      await addGuest();
+    }
+  };
 
   return (
-    <div>
-      <form onSubmit={handleAddGuest}>
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={handleFirstNameChange}
-        />
-        <label htmlFor="lastName">Last Name</label>
-      </form>
+    <div className="App">
+      <h1>Guest List</h1>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <div>
+            <label htmlFor="firstName">First name</label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={handleLastNameChange}
+              onKeyPress={handleKeyPress}
+            />
+          </div>
+
+          {guests.map((guest) => (
+            <div key={guest.id} data-test-id="guest">
+              <span>
+                {guest.firstName} {guest.lastName}
+              </span>
+              <button
+                onClick={() => deleteGuest(guest.id)}
+                aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+              >
+                Remove
+              </button>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={guest.attending}
+                  onChange={() => toggleAttending(guest.id)}
+                  aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+                />
+                Attending
+              </label>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
+
 export default App;
